452f4690defcbe550ce928c50371d264af6d8b46
Runniersoaoi <matiasaquinozc@gmail.com>
2024-11-07 14:56:44 -0500

    feat: add social networks to project

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,26 +1,42 @@
 <template>
   <header
-    class="col-start-2 col-end-6 flex justify-between h-16 items-center px-5 max-w-7xl sticky top-0 z-50 border-b-2 bg-black-bg-principal border-gray-principal"
+    class="col-start-1 col-span-6 flex h-16 justify-center items-center w-full sticky top-0 z-50 border-b-2 bg-black-bg-principal border-gray-principal"
   >
-    <div>
-      <NuxtLink to="/home"
-        ><img src="/image/acm-continental-logo-white.png" alt=""
-      /></NuxtLink>
-    </div>
-    <ul class="flex gap-8 text-sm">
-      <!-- <li v-for="pageLink in pageLinkData" :key="pageLink?.id">
+    <div class="min-w-[1280px] flex justify-between items-center">
+      <div>
+        <NuxtLink to="/home"
+          ><img src="/image/acm-continental-logo-white.png" alt=""
+        /></NuxtLink>
+      </div>
+      <ul class="flex gap-8 text-sm">
+        <!-- <li v-for="pageLink in pageLinkData" :key="pageLink?.id">
         <NuxtLink :to="`/${pageLink.pageLink}`">{{
           pageLink?.pageName
         }}</NuxtLink>
       </li> -->
-      <li><NuxtLink to="/home">Inicio</NuxtLink></li>
+        <li><NuxtLink to="/home">Inicio</NuxtLink></li>
 
-      <!-- <li><NuxtLink to="/blog">Blog</NuxtLink></li> -->
-      <li><NuxtLink to="/comunity">Comunidad</NuxtLink></li>
-      <li><NuxtLink to="/events">Eventos</NuxtLink></li>
-      <li><NuxtLink to="/about">Sobre nosotros</NuxtLink></li>
-    </ul>
-    <div>Images</div>
+        <!-- <li><NuxtLink to="/blog">Blog</NuxtLink></li> -->
+        <li><NuxtLink to="/comunity">Comunidad</NuxtLink></li>
+        <li><NuxtLink to="/events">Eventos</NuxtLink></li>
+        <li><NuxtLink to="/about">Sobre nosotros</NuxtLink></li>
+      </ul>
+      <div class="flex gap-2">
+        <a
+          href="https://www.facebook.com/profile.php?id=61566246254675"
+          target="_blanck"
+          ><icons-facebook-icon
+        /></a>
+        <a
+          href="https://www.instagram.com/acmsistemas2?igsh=cGw5Z2Z5aGoyZGZw&utm_source=qr"
+          target="_black"
+          ><icons-instagram-icon
+        /></a>
+        <a href="https://www.tiktok.com/@acmconti" target="_blanck"
+          ><icons-tiktok-icon
+        /></a>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -34,4 +50,11 @@
 // fetchHomeData();
 </script>
 
-<style scoped></style>
+<style scoped>
+.router-link-active {
+  @apply underline;
+}
+.router-link-exact-active {
+  @apply underline;
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -16,12 +16,26 @@
       <div
         class="w-full h-11 flex bg-black-bg-principal border-t border-gray-border text-white items-center justify-center"
       >
-        <div class="min-w-[1280px] flex justify-between">
+        <div class="min-w-[1280px] flex justify-between items-center">
           <p class="text-sm font-light">
             Copyright ©2024 ACM-Universidad Continental 
           </p>
 
-          <p>redes sociales</p>
+          <div class="flex gap-2">
+            <a
+              href="https://www.facebook.com/profile.php?id=61566246254675"
+              target="_blanck"
+              ><icons-facebook-icon
+            /></a>
+            <a
+              href="https://www.instagram.com/acmsistemas2?igsh=cGw5Z2Z5aGoyZGZw&utm_source=qr"
+              target="_black"
+              ><icons-instagram-icon
+            /></a>
+            <a href="https://www.tiktok.com/@acmconti" target="_blanck"
+              ><icons-tiktok-icon
+            /></a>
+          </div>
         </div>
       </div>
     </div>
